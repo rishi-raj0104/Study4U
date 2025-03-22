89e1e311064ee2a71739ed8a79192433b166aa29
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-03-22 12:23:07 +0530

    error rsolve

--- src/components/core/Homepage/ExploreMore.jsx
+++ src/components/core/Homepage/ExploreMore.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HomePageExplore } from "../../../data/Homepage-explore";
+import { HomePageExplore } from "../../../data/homepage-explore";
 import CourseCard from "./CourseCard";
 import HighlightText from "./HighlightText";
 
